0cbf2db654dc20d5f83d4bf15ba4137291c97432
jiwentao <1043238267@qq.com>
2020-02-23 21:03:20 +0800

    异常、类和继承： 1.从一个异常类派生出另一个 2.可以在类定义中嵌套异常类声明来组合异常 3.这种嵌套声明本身可被继承，还可做基类

---
+++ 2020.2.23/2020.2.23/sales.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"sales.h"
+using std::string;
+
+Sales::bad_index::bad_index(int ix,const string & s)
+	: std::logic_error(s),bi(ix) {}
+Sales::Sales(int yy)
+{
+	year = yy;
+	for(int i = 0; i < MONTHS; i++)
+		gross[i] = 0;
+}
+
+Sales::Sales(int yy, const double * gr, int n)
+{
+	year = yy;
+	int lim = (n < MONTHS) ? n : MONTHS;
+	int i;
+	for(i = 0; i < lim; i++)
+		gross[i] = gr[i];
+	for( ; i < MONTHS; i++)
+		gross[i] = 0;
+}
+
+double Sales::operator[](int i) const
+{
+	if(i < 0 || i >= MONTHS)
+		throw bad_index(i);
+	return gross[i];
+}
+
+double & Sales::operator[](int i)
+{
+	if(i < 0 || i >= MONTHS)
+		throw bad_index(i);
+	return gross[i];
+}
+
+LabeledSales::nbad_index::nbad_index(const string & lb,int ix,
+	const string & s) : Sales::bad_index(ix,s)
+{
+	lbl = lb;
+}
+
+LabeledSales::LabeledSales(const string & lb,int yy)
+	: Sales(yy)
+{
+	lable = lb;
+}
+
+LabeledSales::LabeledSales(const string & lb,int yy,
+	const double * gr, int n) : Sales(yy,gr,n)
+{
+	lable = lb;
+}
+
+double LabeledSales::operator[](int i) const
+{
+	if(i < 0 || i > MONTHS)
+		throw nbad_index(Lable(),i);
+	return Sales::operator[](i);
+}
+
+double & LabeledSales::operator[](int i)
+{
+	if(i < 0 || i > MONTHS)
+		throw nbad_index(Lable(),i);
+	return Sales::operator[](i);
+}
